19880029047f5898d687ec79cdd5d85eed22d496
tryhardkip <akipnsr@gmail.com>
2026-08-26 19:26:47 +0800

    Modify mk file for Matrixx device configuration
    Updated product details and configuration for Matrixx device.

--- matrixx_mh2lm.mk
+++ matrixx_mh2lm.mk
@@ -8,16 +8,31 @@
 $(call inherit-product, device/lge/mh2lm/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/matrixx/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_mh2lm
+PRODUCT_NAME := matrixx_mh2lm
 PRODUCT_DEVICE := mh2lm
 PRODUCT_MANUFACTURER := LGE
 PRODUCT_BRAND := LGE
 PRODUCT_MODEL := G8X ThinQ
 
 PRODUCT_GMS_CLIENTID_BASE := android-lge
+
+# Project Matrixx
+MATRIXX_MAINTAINER := Xtrakari
+TARGET_CUSTOM_UDFPS := true
+TARGET_INCLUDE_PIXEL_LAUNCHER := true
+TARGET_DEFAULT_PIXEL_LAUNCHER := true
+WITH_GMS := false
+WITH_GMS_AICORE := flase
+WITH_GMS_COMMS_SUITE := false
+TARGET_INCLUDE_MATLOG := true
+WITH_BCR := true
+USE_REALITY_ENGINE := true
+SURFACE_FLINGER_BOOST := true
+TARGET_OPTIMIZED_DEXOPT := true
+PRODUCT_NO_CAMERA := false
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     DeviceProduct=mh2lm \
